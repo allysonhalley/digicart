24d6c12d056971f0cd2a25c2d7991f4863ca9a1a
allysonhalley <isolation85@gmail.com>
2017-05-05 17:54:59 -0300

    dashboard incompleto

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :items
   resources :sectors
-  root to: 'items#index'
-  get 'dashboard_items', to: 'items#dashboard_items'
+  root to: 'dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
