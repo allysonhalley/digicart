d874313ebad9fa2925bb02219ba3be559d13d553
allysonhalley <isolation85@gmail.com>
2017-05-05 17:17:23 -0300

    criação do dashboard. falha na renderização da tela. refazendo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :items
+  resources :sectors
+  root to: 'items#index'
+  get 'dashboard_items', to: 'items#dashboard_items'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
