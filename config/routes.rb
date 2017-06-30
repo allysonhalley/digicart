2d82687349507cda0a18953e5b99d7d8d76dfc9d
allysonhalley <isolation85@gmail.com>
2017-06-29 23:13:18 -0300

    Insersão de Movemente pronto pra ser chamado pelo app Controlador: Movements Método: to_move(bar_code) route: post 'to_move', to: 'movements#to_move', as: :to_move

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :types
   resources :statuses
 
+  post 'to_move', to: 'movements#to_move', as: :to_move
   get 'dashboard', to: 'movements#dashboard', as: :dashboard
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
